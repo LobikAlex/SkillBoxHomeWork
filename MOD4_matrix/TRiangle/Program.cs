7aeeb4b0ef95aaf60257a91cefc04b6a03663e0f
LobikAlex <92232278+LobikAlex@users.noreply.github.com>
2021-10-25 01:08:35 +0400

    Add project

---
+++ MOD4_matrix/TRiangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Треугольник Паскаля");
+            Console.Write("Введите n: ");               // Приглашение ко вводу числа
+            int n = int.Parse(Console.ReadLine());      // Ввод числа с клавиатуры
+
+            int[,] pascal = new int[n, n];
+
+            pascal[0, 0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+               //добавляем табы
+               for (int k = 0; k < (n-i); k++)   
+                {
+                    Console.Write("\t");
+                }
+
+               //Заполняем треугольник
+                for (int j = 1; j < n; j++)
+                {
+                    if (i < j)  {break;}
+                    pascal[i, j] = pascal[i - 1, j] + pascal[i - 1, j - 1];
+                    Console.Write($"{pascal[i, j]}\t\t");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MOD4_matrix/mod4_z1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MOD4_Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Учёт финансов");
+
+            //объявляем массивы для кредита, дебета и выручки
+            int[] credit = new int[12];
+            int[] debet = new int[12];
+            int[] sum = new int[12];
+            int plus = 0;
+            Random RND = new Random();
+
+            Console.WriteLine("месяц\tКредит\tДебет\tВыручка");
+           
+
+            //заполняем массивы
+            for (int i = 0; i < 12; i++)
+            {
+                credit[i] = RND.Next(30, 100) * 10;
+                debet[i] = RND.Next(10, 70) * 10;
+                sum[i] = credit[i] - debet[i];
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}",i, credit[i], debet[i], sum[i]);
+                
+                
+                //вычисляем количество месяцев с положительным балансом
+                if (sum[i] > 0)
+                {
+                  plus = ++plus;
+                }
+                
+            }
+            Console.WriteLine("Количество положительных месяцев: " + plus);
+            Array.Sort(sum);
+            foreach (var i in sum)
+            {
+                Console.WriteLine($"{i}\t");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+// НЕ решена задача вывода худших месяцев:                  Худшая прибыль в месяцах: 7, 4, 1, 5, 12
