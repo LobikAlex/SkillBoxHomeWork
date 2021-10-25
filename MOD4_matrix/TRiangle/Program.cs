ff61635f68c1a75e08c5aba83ce5fa5a7d710998
LobikAlex <92232278+LobikAlex@users.noreply.github.com>
2021-10-26 02:49:24 +0400

    Изменил табы на пробелы в треугольнике паскаля

--- MOD4_matrix/TRiangle/Program.cs
+++ MOD4_matrix/TRiangle/Program.cs
@@ -9,16 +9,16 @@ namespace Triangle
             Console.WriteLine("Треугольник Паскаля");
             Console.Write("Введите n: ");               // Приглашение ко вводу числа
             int n = int.Parse(Console.ReadLine());      // Ввод числа с клавиатуры
-
+            
             int[,] pascal = new int[n, n];
 
             pascal[0, 0] = 1;
             for (int i = 1; i < n; i++)
             {
-               //добавляем табы
+               //вместо табов ставим пробелы
                for (int k = 0; k < (n-i); k++)   
                 {
-                    Console.Write("\t");
+                    Console.Write("   ");
                 }
 
                //Заполняем треугольник
@@ -26,7 +26,7 @@ namespace Triangle
                 {
                     if (i < j)  {break;}
                     pascal[i, j] = pascal[i - 1, j] + pascal[i - 1, j - 1];
-                    Console.Write($"{pascal[i, j]}\t\t");
+                    Console.Write($"{pascal[i, j]}      ");
                 }
                 Console.WriteLine("\n");
             }
