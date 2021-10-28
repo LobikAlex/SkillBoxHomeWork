9d7fbaf07e6faf7d3083d34c6987bac73b186b5f
LobikAlex <92232278+LobikAlex@users.noreply.github.com>
2021-10-28 04:02:50 +0400

    Доделана финансовая таблица

--- MOD4_matrix/mod4_z1/Program.cs
+++ MOD4_matrix/mod4_z1/Program.cs
@@ -12,6 +12,7 @@ namespace MOD4_Z1
             int[] credit = new int[12];
             int[] debet = new int[12];
             int[] sum = new int[12];
+            int[] month = new int[12];      // массив с месяцами
             int plus = 0;
             Random RND = new Random();
 
@@ -21,28 +22,45 @@ namespace MOD4_Z1
             //заполняем массивы
             for (int i = 0; i < 12; i++)
             {
+                month[i] = i+1;
                 credit[i] = RND.Next(30, 100) * 10;
                 debet[i] = RND.Next(10, 70) * 10;
                 sum[i] = credit[i] - debet[i];
-                Console.WriteLine("{0}\t{1}\t{2}\t{3}",i, credit[i], debet[i], sum[i]);
-                
-                
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}",i+1, credit[i], debet[i], sum[i]);              
+              
                 //вычисляем количество месяцев с положительным балансом
                 if (sum[i] > 0)
                 {
                   plus = ++plus;
-                }
-                
+                }                
             }
             Console.WriteLine("Количество положительных месяцев: " + plus);
-            Array.Sort(sum);
-            foreach (var i in sum)
+
+
+
+            //
+            Array.Sort(sum, month);
+            
+            
+            Console.WriteLine("Худшие месяцы:");
+
+
+            int k = 0;
+            int m = 3;
+            
+            // вычисляем 3 худшие месяца по условию задачи
+            while (k < m)
             {
-                Console.WriteLine($"{i}\t");
+            Console.WriteLine($"{month[k]} месяц. Выручка: {sum[k]}");
+            k++;
+            
+                if (sum[k] == sum[k-1])              // если выручка по месяцам одинаковая, то 
+                {
+                m++;                                 // отодвигием счетчик
+                }               
             }
 
             Console.ReadKey();
         }
     }
 }
-// НЕ решена задача вывода худших месяцев:                  Худшая прибыль в месяцах: 7, 4, 1, 5, 12
